64d59815fc73b8df1dd15a8060376be1ebe1ff4f
Matt Gregory <matt.gregory@oregonstate.edu>
2023-10-19 17:04:50 -0700

    Refactor tests to reduce duplication and move data into tests module

--- tests/test_model_fc.py
+++ tests/test_model_fc.py
@@ -2,127 +2,75 @@ import pytest
 
 from geeknn.ordination import GNN, MSN, Euclidean, Mahalanobis, Raw
 from geeknn.ordination.utils import Colocation
-from geeknn.test_data import setup as sample_data
 
+from .setup import colocation_fc, env_columns, fc, id_field, spp_columns
 
-@pytest.fixture()
-def k():
-    return 5
+ESTIMATOR_PARAMETERS = {
+    "raw": (Raw, {}),
+    "euc": (Euclidean, {}),
+    "mah": (Mahalanobis, {}),
+    "msn": (MSN, {}),
+    "gnn": (GNN, {"spp_transform": "SQRT", "num_cca_axes": 16}),
+}
 
 
 @pytest.fixture()
 def training_data():
     return {
-        "fc": sample_data.fc,
-        "id_field": sample_data.id_field,
-        "spp_columns": sample_data.spp_columns,
-        "env_columns": sample_data.env_columns,
+        "fc": fc,
+        "id_field": id_field,
+        "spp_columns": spp_columns,
+        "env_columns": env_columns,
     }
 
 
 @pytest.fixture()
 def colocation_obj():
-    return Colocation(
-        fc=sample_data.colocation_fc, location_field="LOC_ID", plot_field="FCID"
-    )
+    return Colocation(fc=colocation_fc, location_field="LOC_ID", plot_field="FCID")
+
+
+@pytest.fixture()
+def observed_ids():
+    return fc.limit(10).aggregate_array("FCID").getInfo()
 
 
 def run_method(kls, options, training_data, colocation_obj=None):
-    # Create the model
-    model = kls(**options)
-
-    # Train the model
-    model = model.train(**training_data)
-
-    # Classify the first 10 records of the test feature collection
+    model = kls(**options).train(**training_data)
     return model.predict_fc(
-        fc=sample_data.fc.limit(10),
+        fc=fc.limit(10),
         colocation_obj=colocation_obj,
     )
 
 
-def get_obs_ids(fc, id_field="FCID", limit=10):
-    return fc.limit(limit).aggregate_array(id_field).getInfo()
-
-
-def test_raw_dependent(k, training_data):
-    neighbors = run_method(Raw, {"k": k}, training_data)
-    obs = get_obs_ids(training_data["fc"])
+@pytest.mark.parametrize(
+    "estimator_parameter",
+    ESTIMATOR_PARAMETERS.values(),
+    ids=ESTIMATOR_PARAMETERS.keys(),
+)
+@pytest.mark.parametrize("k", [5])
+def test_dependent(estimator_parameter, k, training_data, observed_ids):
+    """Test that estimators when run without a colocation object return
+    itself as the first neighbor"""
+    est, options = estimator_parameter
+    options["k"] = k
+    neighbors = run_method(est, options, training_data)
     prd = [x[0] for x in neighbors.getInfo()]
-    assert all(x == y for x, y in zip(obs, prd))
+    assert all(x == y for x, y in zip(observed_ids, prd))
 
 
-def test_euc_dependent(k, training_data):
-    neighbors = run_method(Euclidean, {"k": k}, training_data)
-    obs = get_obs_ids(training_data["fc"])
+@pytest.mark.parametrize(
+    "estimator_parameter",
+    ESTIMATOR_PARAMETERS.values(),
+    ids=ESTIMATOR_PARAMETERS.keys(),
+)
+@pytest.mark.parametrize("k", [5])
+def test_independent(
+    estimator_parameter, k, training_data, colocation_obj, observed_ids
+):
+    """Test that estimators when run with a colocation object do not
+    return itself as the first neighbor"""
+    est, options = estimator_parameter
+    options["k"] = k
+    neighbors = run_method(est, options, training_data, colocation_obj=colocation_obj)
     prd = [x[0] for x in neighbors.getInfo()]
-    assert all(x == y for x, y in zip(obs, prd))
-
-
-def test_mah_dependent(k, training_data):
-    neighbors = run_method(Mahalanobis, {"k": k}, training_data)
-    obs = get_obs_ids(training_data["fc"])
-    prd = [x[0] for x in neighbors.getInfo()]
-    assert all(x == y for x, y in zip(obs, prd))
-
-
-def test_msn_dependent(k, training_data):
-    neighbors = run_method(MSN, {"k": k}, training_data)
-    obs = get_obs_ids(training_data["fc"])
-    prd = [x[0] for x in neighbors.getInfo()]
-    assert all(x == y for x, y in zip(obs, prd))
-
-
-def test_gnn_dependent(k, training_data):
-    neighbors = run_method(
-        GNN,
-        {"k": k, "spp_transform": "SQRT", "num_cca_axes": 16},
-        training_data,
-    )
-    obs = get_obs_ids(training_data["fc"])
-    prd = [x[0] for x in neighbors.getInfo()]
-    assert all(x == y for x, y in zip(obs, prd))
-
-
-def test_raw_independent(k, training_data, colocation_obj):
-    neighbors = run_method(Raw, {"k": k}, training_data, colocation_obj=colocation_obj)
-    obs = get_obs_ids(training_data["fc"])
-    prd = [x[0] for x in neighbors.getInfo()]
-    assert all(x != y for x, y in zip(obs, prd))
-
-
-def test_euc_independent(k, training_data, colocation_obj):
-    neighbors = run_method(
-        Euclidean, {"k": k}, training_data, colocation_obj=colocation_obj
-    )
-    obs = get_obs_ids(training_data["fc"])
-    prd = [x[0] for x in neighbors.getInfo()]
-    assert all(x != y for x, y in zip(obs, prd))
-
-
-def test_mah_independent(k, training_data, colocation_obj):
-    neighbors = run_method(
-        Mahalanobis, {"k": k}, training_data, colocation_obj=colocation_obj
-    )
-    obs = get_obs_ids(training_data["fc"])
-    prd = [x[0] for x in neighbors.getInfo()]
-    assert all(x != y for x, y in zip(obs, prd))
-
-
-def test_msn_independent(k, training_data, colocation_obj):
-    neighbors = run_method(MSN, {"k": k}, training_data, colocation_obj=colocation_obj)
-    obs = get_obs_ids(training_data["fc"])
-    prd = [x[0] for x in neighbors.getInfo()]
-    assert all(x != y for x, y in zip(obs, prd))
-
-
-def test_gnn_independent(k, training_data, colocation_obj):
-    neighbors = run_method(
-        GNN,
-        {"k": k, "spp_transform": "SQRT", "num_cca_axes": 16},
-        training_data,
-        colocation_obj=colocation_obj,
-    )
-    obs = get_obs_ids(training_data["fc"])
-    prd = [x[0] for x in neighbors.getInfo()]
-    assert all(x != y for x, y in zip(obs, prd))
+    assert all(x != y for x, y in zip(observed_ids, prd))
