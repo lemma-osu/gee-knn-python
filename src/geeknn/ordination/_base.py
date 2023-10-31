98420f7a21ed52c2d6bbd92ac074425c1ff4b231
Matt Gregory <matt.gregory@oregonstate.edu>
2023-10-30 17:29:16 -0700

    Add client-side proxies for Earth Engine objects
    In order to run ordinations client-side, there needs to be a mechanism
    for translating `ee.FeatureCollections` to python objects (in this case,
    pydantic models).  Based on work from @aazuspan, these classes provide
    multi-threaded functionality to convert feature collections in a
    lower-memory context.  `Feature` and `FeatureCollection` have a few
    helper functions to extract properties as values and lists.

--- src/geeknn/ordination/_base.py
+++ src/geeknn/ordination/_base.py
@@ -1,3 +1,67 @@
+from __future__ import annotations
+
+from itertools import chain
+from typing import Any
+
+import ee
+from joblib import Parallel, delayed
+from pydantic import BaseModel
+
+
+class Geometry(BaseModel):
+    """Client-side proxy for ee.Geometry object"""
+
+    type: str
+    coordinates: list[Any]
+
+
+class Feature(BaseModel):
+    """Client-side proxy for ee.Feature object"""
+
+    type: str
+    geometry: Geometry
+    id: str
+    properties: dict[str, Any]
+
+    def get_property(self, property: str) -> Any:
+        return self.properties.get(property)
+
+    def get_properties(self, properties: list[str]) -> list[Any]:
+        return [self.get_property(x) for x in properties]
+
+
+class FeatureCollection(BaseModel):
+    """Client-side proxy for ee.FeatureCollection object"""
+
+    type: str
+    columns: dict[str, Any]
+    version: int
+    id: str
+    properties: dict[str, Any]
+    features: list[Feature]
+
+    def aggregate_array(self, property: str) -> list[Any]:
+        return [f.get_property(property) for f in self.features]
+
+    def properties_to_array(self, properties: list[str]) -> list[list[Any]]:
+        return [f.get_properties(properties) for f in self.features]
+
+    @classmethod
+    def from_ee_feature_collection(
+        cls, fc: ee.FeatureCollection, num_threads: int = -1, chunk_size: int = 5000
+    ):
+        """Create a client-side FeatureCollection from an ee.FeatureCollection
+        using multiple threads and chunking to avoid memory issues.
+        """
+        size = fc.size().getInfo()
+        info = fc.limit(0).getInfo()
+        chunks = [fc.toList(chunk_size, i) for i in range(0, size, chunk_size)]
+        with Parallel(n_jobs=num_threads, backend="threading") as p:
+            chunk_data = p(delayed(chunk.getInfo)() for chunk in chunks)
+        info.update({"features": list(chain.from_iterable(chunk_data))})
+        return cls(**info)
+
+
 class GeeKnnClassifier:
     def __init__(self, k=1, max_duplicates=None):
         self.k = k
